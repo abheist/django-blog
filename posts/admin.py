473e77ed8eb85330f627fc0380bc773b2fd3e456
Abhishek Kumar Singh <abhishek@kisanhub.com>
2016-12-27 18:07:21 +0530

    AKS: some changes in admin panel

--- posts/admin.py
+++ posts/admin.py
@@ -5,7 +5,8 @@ from .models import Post
 
 class PostModelAdmin(admin.ModelAdmin):
     list_display = ["title", "timestamp", "updated"]
-    list_display_links = ["title", "timestamp", "updated"]
+    list_display_links = ["timestamp", "updated"]
+    list_editable = ["title"]
     list_filter = ["timestamp", "updated"]
     search_fields = ["title", "content"]
 
