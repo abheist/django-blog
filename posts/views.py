33c6f0d9696c70a91bffdb3a1497204e811ba226
Abhishek Kumar Singh <abhishek@kisanhub.com>
2016-12-27 19:29:53 +0530

    AKS: change in urls

--- posts/views.py
+++ posts/views.py
@@ -5,15 +5,24 @@ from django.shortcuts import render
 def posts_create(request):
     return HttpResponse("<h1>Create</h1>")
 
+
 def posts_detail(request):
-    return HttpResponse("<h1>Detail</h1>")
+    context = {
+        "title": "Detail"
+    }
+    return render(request, "index.html", context)
+
 
 def posts_list(request):
-    # return HttpResponse("<h1>List</h1>")
-    return render(request, "index.html", {})
+    context = {
+        "title": "List"
+    }
+    return render(request, "index.html", context)
+
 
 def posts_update(request):
     return HttpResponse("<h1>Update</h1>")
+
 
 def posts_delete(request):
     return HttpResponse("<h1>Delete</h1>")
