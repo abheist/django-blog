810fdf2b510b794a0e95007c14d427a00595528b
Abhishek Kumar Singh <abhishek@kisanhub.com>
2016-12-27 18:03:54 +0530

    AKS: Intermediate - admin panel

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ["title", "timestamp", "updated"]
+    list_display_links = ["title", "timestamp", "updated"]
+    list_filter = ["timestamp", "updated"]
+    search_fields = ["title", "content"]
+
+    class Meta:
+        model = Post
+
+
+admin.site.register(Post, PostModelAdmin)
