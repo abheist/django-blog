a6bc1cb8f56bd7773f4ca6ab824e398f1dc9d6c4
Abhishek Kumar Singh <abhishek@kisanhub.com>
2016-12-28 16:52:51 +0530

    AKS: change in views and urls

--- posts/views.py
+++ posts/views.py
@@ -1,20 +1,31 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post
 
 
 def posts_create(request):
     return HttpResponse("<h1>Create</h1>")
 
 
-def posts_detail(request):
+def posts_detail(request, id=None):
+    instance = get_object_or_404(Post, id=id)
     context = {
-        "title": "Detail"
+        "title": "Detail",
+        "instance": instance
     }
-    return render(request, "index.html", context)
+    return render(request, "post_detail.html", context)
 
 
 def posts_list(request):
+    queryset = Post.objects.all()
+    # if request.user.is_authenticated():
+    #     context = {
+    #         "title": "My User list"
+    #     }
+    # else:
     context = {
+        "object_list": queryset,
         "title": "List"
     }
     return render(request, "index.html", context)
