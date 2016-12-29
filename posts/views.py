f3534daaa5f404b61b10ab58d804837c41878580
Abhishek Kumar Singh <abhishek@kisanhub.com>
2016-12-29 12:09:15 +0530

    AKS: Intermediate

--- posts/views.py
+++ posts/views.py
@@ -8,7 +8,7 @@ from .models import Post
 
 
 def posts_create(request):
-    form = PostForm(request.POST or None)
+    form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
@@ -50,7 +50,7 @@ def posts_list(request):
 
 def posts_update(request, id=None):
     instance = get_object_or_404(Post, id=id)
-    form = PostForm(request.POST or None, instance=instance)
+    form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
