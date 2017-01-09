323f3236bc587e59ee1bc66dbf6324fb35f7d6e0
Abhishek Kumar Singh <abhishek@kisanhub.com>
2017-01-09 12:30:53 +0530

    AKS: completed 1.9 tut

--- posts/urls.py
+++ posts/urls.py
@@ -8,19 +8,10 @@ from .views import (
     post_delete
 )
 
-# urlpatterns = [
-#     url(r'^$', posts_list, name='list'),
-#     url(r'^create/$', posts_create),
-#     url(r'^(?P<slug>\d+)/$', posts_detail, name='detail'),
-#     url(r'^(?P<slug>\d+)/edit$', posts_update, name='update'),
-#     url(r'^(?P<slug>\d+)/delete/$', posts_delete)
-# ]
-
 urlpatterns = [
     url(r'^$', post_list, name='list'),
     url(r'^create/$', post_create),
     url(r'^(?P<slug>[\w-]+)/$', post_detail, name='detail'),
     url(r'^(?P<slug>[\w-]+)/edit/$', post_update, name='update'),
     url(r'^(?P<slug>[\w-]+)/delete/$', post_delete),
-    # url(r'^posts/$', "<appname>.views.<function_name>"),
 ]
